c4980d2503e69f24506a957bacd238ba5229d816
RomulloOliveira <felipe2006__@hotmail.com>
2024-05-01 16:47:51 -0300

    add js e mask

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+const orçamento= document.querySelector(".div-orçamento")
+const mascara= document.querySelector(".mascara-background")
+
+function CliqueiNoBotao(){
+
+    orçamento.style.left= "50%"
+    orçamento.style.transfom= "translateX(-50%)"
+    mascara.style.visibility= "visible"
+
+
+}
+
+function Cliquefora(){
+    
+    orçamento.style.left= "-300px"
+    orçamento.style.transfom= "translateX(0)"
+    mascara.style.visibility= "hidden"
+}
